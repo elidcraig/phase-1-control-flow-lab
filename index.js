30d0fc4e0316dd7ea30397d515d88b22bbd55676
Eli Craig <elidcraig@gmail.com>
2022-04-25 14:50:22 -0700

    commit tree 257674c8eaf1982b117c77f5d9e9940328bc7295

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  let message  
+  if (length <= 400) {
+      message = 'This one is on me!'
+    }
+  else if (length <= 2000) {
+    message = 'That will be twenty bucks.'
+  }
+  else if (length <= 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    message = 'No can do.'
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message
+  if (city === 'NYC') {
+    message = 'Ok, sounds good.'
+  }
+  else {
+    message = 'No go.'
+  }
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.'
+      return message;
+    case 'not as generous':
+      message = 'Thank you.'
+      return message;
+    default:
+      message = 'Bye.'
+      return message;
+  }
 }
